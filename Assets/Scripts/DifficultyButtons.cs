073c17d8aceddd44a5c1a2aa5f4e9ad5fb43a17b
Illia <B00165280@mytudublin.ie>
2024-11-28 19:43:24 +0000

    Finished challenge 5

---
+++ Assets/Scripts/DifficultyButtons.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class DifficultyButtons : MonoBehaviour
+{
+    private Button button;
+    private GameManager gameManager;
+    public int difficulty;
+    // Start is called before the first frame update
+    void Start()
+    {
+        button = GetComponent<Button>();
+        button.onClick.AddListener(SetDifficulty);
+    }
+    void SetDifficulty()
+    {
+        Debug.Log(gameObject.name + " was clicked");
+        gameManager.StartGame(difficulty);
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
